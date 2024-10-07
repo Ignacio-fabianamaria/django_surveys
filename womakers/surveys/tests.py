dae0994e5a426a9d4672c5fa8da6d5bcafb4bebd
Ignacio-fabianamaria <faby.tbe@gmail.com>
2024-10-07 12:31:35 -0300

    test: add tests for Vote Model

--- womakers/surveys/tests.py
+++ womakers/surveys/tests.py
@@ -1,37 +1,39 @@
 # https://docs.djangoproject.com/en/5.1/topics/testing/overview/
 
 from django.test import TestCase
-from .models import Question, Option
+from .models import Question, Option, Vote
 from datetime import date
 
 
 class BaseTestCase(TestCase):
     def setUp(self):
         self.question = Question.objects.create(text="Qual sua cor favorita?")
+        self.option1 = Option.objects.create(text="verde", question=self.question)
+        self.option2 = Option.objects.create(text="vermelho", question=self.question)
 
 
-class QuestionTestCase(TestCase):
+class QuestionTestCase(BaseTestCase):
     def test_question_creation(self):
         self.assertEqual(self.question.text, "Qual sua cor favorita?")
         self.assertEqual(self.question.created_at.date(), date.today())
         self.assertTrue(self.question.enabled)
-
-    def test_question_string(self):
         self.assertEqual(str(self.question), "Qual sua cor favorita?")
 
 
-class OptionTestCase(TestCase):
+class OptionTestCase(BaseTestCase):
     def test_option_creation(self):
-        option1 = Option.objects.create(text="verde", question=self.question)
-        option2 = Option.objects.create(text="vermelho", question=self.question)
 
-        self.assertEqual(option1.text, "verde")
-        self.assertEqual(option2.text, "vermelho")
-        self.assertEqual(option1.question, self.question)
-        self.assertEqual(option2.question, self.question)
+        self.assertEqual(self.option1.text, "verde")
+        self.assertEqual(self.option2.text, "vermelho")
+        self.assertEqual(self.option1.question, self.question)
+        self.assertEqual(self.option2.question, self.question)
         self.assertEqual(self.question.option_set.count(), 2)
+        self.assertEqual(str(self.option1), "verde")
 
-    def test_question_string(self):
-        option = Option.objects.create(text="verde", question=self.question)
 
-        self.assertEqual(str(option), "verde")
+class VoteTestCase(BaseTestCase):
+    def test_vote_creation(self):
+        vote = Vote.objects.create(option=self.option1)
+
+        self.assertEqual(vote.option, self.option1)
+        self.assertEqual(Vote.objects.count(), 1)
